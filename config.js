9988a893e8951d9eb02b5f80d7a2a6d036fb71aa
Lucian <tsijibomi@gmail.com>
2023-12-10 17:36:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['09069690635', 'GURU', true],
+  ['09069690635', 'LUCIAN', true], 
   [''] 
 ] //Number of owners
 
